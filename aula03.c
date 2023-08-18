b3549c3cf41b9109e760e990f0a7d580e5e98b24
LethalFrames <141968251+LethalFrames@users.noreply.github.com>
2023-08-17 21:38:03 -0300

    Update aula03.c

--- aula03.c
+++ aula03.c
@@ -22,6 +22,37 @@ int idade,x,y,z,media;
   scanf("%d %d %d", & x, & y, & z);
   media = (x + y + z)/3;
   printf("Sua media e: %d",media);
+
+/*
+  A linguagem C possui 4 tipos de dados primitivos.
+  cada um armazena um tipo de informacao e possui um tamanho diferente em bytes.
   
+  Char   1 bytes Armazena caracteres.
+  int    4 bytes Armazena numeros inteiro.
+  float  4 bytes Armazena numeros reais ate 6 casas decimais.
+  double 8 bytes Armazena numeros reais ate 15 casas decimais.
+*/
+  char minhaletra = 'C';
+  int copaBrasil = 1991;
+  float floatpi = 3.14159265358979323846;
+  double doublepi = 3.14159265358979323846;
+  printf("Caracter: \t\t%c \t\t\t\t\tTamanho %lu byte(s).\n" ,minhaletra, sizeof(minhaletra));
+  printf ("Valor int: \t\t%i\t\t\t\tTamanho %lu byte(s).\n", copaBrasil, sizeof(copaBrasil));
+  printf("Valor float: \t%.6f\t\t\tTamanho %lu byte(s).\n", floatpi, sizeof (floatpi));
+  printf("Valor double: \t%.8f\t\t\tTamanho %lu byte(s).\n", doublepi, sizeof (doublepi));
+
+  /*
+A linguagem C possui 4 tipos de modificadores de tipo.
+Estes modificadores definem um novo tamanho de sinal para as variaves
+  
+  short    Divide na metade o tamanho de uma variavel.
+  long     Dobra o tamanho de uma variavel.
+  signed   Define como numeros apenas positivos.
+  unsigned Define como numeros positivos e negativos.
+  */
+/*
+signed int x = 200;
+unsigned int y = -100;
+*/
   return 0;
 }
